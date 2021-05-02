6e796214de08c61fd8a7618f04e4b5da350f4ea4
Gustavo Gonzalez <gustagonme@gmail.com>
2021-05-02 01:58:34 -0500

    setup location model config

---
+++ projectlocation-db/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+
+const setupDatabase = require('./lib/db')
+const setupLocation = require('./lib/location')
+const setupLocationModel = require('./models/location')
+const defaults = require('defaults')
+
+module.exports = async function (config) {
+    config = defaults(config, {
+      dialect: 'sqlite',
+      pool: {
+        max: 10,
+        min: 0,
+        idle: 10000
+      },
+      query: {
+        raw: true
+      }
+    })
+
+const sequelize = setupDatabase(config)
+const LocationModel = setupLocationModel(config)
+
+await sequelize.authenticate()
+
+if (config.setup) {
+    await sequelize.sync({ force: true })
+}
+
+const Location = setupLocation(LocationModel)
+
+return {
+    Location
+}
+
+}
--- projectlocation-db/lib/location.js
+++ projectlocation-db/lib/location.js
@@ -1,5 +1,45 @@
 'use strict'
 
 module.exports = function setupLocation (LocationModel) {
-    
+    function findByUuid(uuid){
+        return LocationModel.findOne({
+            where:{
+                uuid
+            }
+        })
+    }
+
+    function findAll(){
+        return LocationModel.findAll()
+    }
+
+    async function create (location){
+        const cond = {
+            where:{
+                uuid:location.uuid
+            }
+        }
+
+        const existingLocation = await LocationModel.findOne(cond)
+
+        if(existingLocation){
+            let res
+            res = {
+                message: "Ya existe una locación con este id único."
+            }
+            return res.toJson()
+        }
+
+        const result = await LocationModel.create(location)
+        return result.toJson()
+
+
+    }
+
+    return {
+        findAll,
+        create,
+        findByUuid
+    }
+
 }
