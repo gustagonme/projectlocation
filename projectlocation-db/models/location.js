15b43aa8299bcf2cd4081983200f9731e31cc5c6
Gustavo Gonzalez <gustagonme@gmail.com>
2021-05-02 01:39:25 -0500

    changes for setup database config & location model

---
+++ projectlocation-db/models/location.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = function setupLocationModel (config) {
+    const sequelize = setupDatabase(config)
+    let location
+
+    location =  sequelize.define('location', {
+        uuid: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        area_m2: {
+          type: Sequelize.INTEGER,
+          allowNull: false
+        },
+        address: {
+          type: Sequelize.STRING,
+          allowNull: true
+        },
+        parent_location_id: {
+          type: Sequelize.INTEGER,
+          allowNull: true
+        }
+      })
+
+      location.belongsTo(Location, {foreignKey: 'fk_uuid', targetKey: 'parent_location_id'})
+
+      return location
+
+
+}
